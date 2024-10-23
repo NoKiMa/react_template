0c10f1c654baf721391b7e0c4ce44560ebc190e5
NoKi <novak.kirill@gmail.com>
2024-10-23 12:59:51 +0300

    minor changes

--- src/routes/tab.test.tsx
+++ src/routes/tab.test.tsx
@@ -1,10 +1,10 @@
-import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom'
+import {render, screen} from '@testing-library/react';
+import '@testing-library/jest-dom';
 import {describe, it, expect} from 'vitest';
 import Tab from './tab';
 
-describe('MyComponent', () => {
-  it('renders correctly with initial props', () => {
+describe('Tab', () => {
+  it('check UI', () => {
     render(<Tab />); // Adjust the props passed to your component if needed
     expect(screen.getByText(/id/i)).toBeInTheDocument();
   });
